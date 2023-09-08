89ee2f9b7ae743d525744e43323e5d255a740c3c
zhuoyingl3 <zhuoyingl3@student.unimelb.edu.au>
2023-09-09 04:33:39 +1000

    finished responsibility of log-in page

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,5 +1,5 @@
 import React from "react";
- 
+
 import './ButtonStyles.css';
 import './LoginStyles.css';
 import InputForm from '../components/Input.js'
@@ -9,26 +9,23 @@ import { useNavigate } from 'react-router-dom'; //Login.js
 function LoadLogInPage() {
 
   return (
-    <div className="container m-0 fix">
-      <div className="row">
-        <div className="col-sm-8 gx-0">
-          <img
-            src="https://github.com/ITProject-Thu-12pm/Assets/blob/main/mountain.jpg?raw=true"
-            alt="moutain"
-            className="img-responsive log-in-img"
-          />
-        </div>
-        <hr className="d-sm-none" />
+    <div className="container-all">
 
-        <div className="col-sm-4 gx-5 pt-5 pl-3 form-floating">
-          <div className="title loginTitle1 form-floating">
-            Connecting You to
-            <div className="loginTitle2">What Matters Most</div>
+      <div className="container-left">
+        {/* img */}
+      </div>
+
+      <div className="container-right">
+        <form className="content">
+          <div className="header">
+            <h1 className="title loginTitle1">Connecting You to</h1>
+            <h1 className="loginTitle2">What Matters Most</h1>
           </div>
 
           <LogInForm />
           <Buttons />
-        </div>
+
+        </form>
       </div>
     </div>
   );
@@ -37,8 +34,8 @@ function LoadLogInPage() {
 function LogInForm() {
   return (
     <div className="input">
-      <InputForm inputTitle="Email Address" inputType="email"/>
-      <InputForm inputTitle="Password" inputType="password"/>
+      <InputForm inputTitle="Email Address" inputType="email" />
+      <InputForm inputTitle="Password" inputType="password" />
     </div>
   );
 }
@@ -53,19 +50,20 @@ function Buttons() {
   const handleForgotClick = () => {
     navigate("/forgot");
   };
-  return (
-    <form className="input">
 
-      <button type="button" className="btn btn-lighter-secondary w-100 rounded-5 py-2 login">Log in</button>
-      <button type="button" className="btn w-100 rounded-5 py-2 forgot-pass" onClick={handleForgotClick}>Forgot password?</button>
+  return (
+    <div className="btns">
+
+      <button type="button" className="btn-lighter-secondary rounded-5 btn login">Log in</button>
+      <button type="button" className="rounded-5 btn forgot-pass" onClick={handleForgotClick}>Forgot password?</button>
 
       <div className="new-acc">
         <span>Don't have an account?</span>
-        <button type="button" className="btn btn-outline-secondary rounded-5 py-2 sign-up" onClick={handleSignInClick}>Sign up</button>
+        <button type="button" className="btn-outline-secondary rounded-5 sign-up" onClick={handleSignInClick}>Sign up</button>
 
       </div>
 
-    </form>
+    </div>
   );
 }
 

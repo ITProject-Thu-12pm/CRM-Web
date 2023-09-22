04dc539fcc367fc8e843c6c6c4f95d5cc7cdc3bd
yiqunyang <yiquyang@student.unimelb.edu.au>
2023-09-22 21:42:38 +1000

    implement axios reset password

--- src/pages/Interface.js
+++ src/pages/Interface.js
@@ -18,11 +18,37 @@ export async function Login(user_email, user_password) {
             authentication_status = false;
         }
     } catch (error) {
-        console.error("Request Fail: ", error);
+        //console.error("Request Fail: ", error);
         authentication_status = false;
     }
     return authentication_status;
 
 }
 
+export async function Reset_Passowrd(old_password, new_password) {
+    let success = false;
+    
+    try {
+        // connect to the backend and post the
+        const response1 = await axios.post('http://127.0.0.1:8000/user/1@gmail.com/', {
+            old_password: old_password
+            
+        });
+        if (response1.status === 201) {
+             const response2 = await axios.put('http://127.0.0.1:8000/user/1@gmail.com/', {
+             new_password: new_password
+         });
+             if (response2.status === 201) {
+                 console.log("Reset Password Success!");
+                 success = true
+             } else {
+                 console.log("User reset password Fail!");
+             } 
+         }
+    } catch (error) {
+        //console.error("Request Fail: ", error);
+        
+    }
+    return success
 
+}

4f03ea8ca9f27bc34555cd72cdffd01d41b0c3b9
zhuoyingl3 <zhuoyingl3@student.unimelb.edu.au>
2023-10-03 17:08:27 +1100

    Revert "modify btn styles in login"
    This reverts commit d5afa30948f5b7e2c99d0b7e239164ddf89639d5.

--- src/pages/DashnoardPage/Dashboard.js
+++
@@ -1,3 +0,0 @@
-import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
-import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
-import DoneAllIcon from '@mui/icons-material/DoneAll';

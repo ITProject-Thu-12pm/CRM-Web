a20b2edc627795e57e5b432564a95b8785b231f2
Zhuoying Li <aazhuoying@gmail.com>
2023-10-17 08:31:12 +1100

    add verfy vertification code

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -3,36 +3,56 @@ import { useNavigate } from "react-router-dom";
 import InputForm from "../components/Inputs/Input.js";
 import "./ForgotPasswordStyles.css";
 import emailjs from "@emailjs/browser";
+
 function LoadForgotPage() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [verificationCodeSent, setVerificationCodeSent] = useState(false);
+  const [enteredCode, setEnteredCode] = useState("");
+
+  //generate vertification code
+  const [verificationCode] = useState(
+    Math.floor(100000 + Math.random() * 900000).toString()
+  );
+
+  const handleVerifyCode = () => {
+    console.log(verificationCode);
+    if (enteredCode !== verificationCode) {
+      alert(
+        "The verification code you entered is incorrect. Please try again."
+      );
+    } else {
+      //Todo: implement change password here!
+      navigate("/login");
+    }
+  };
+
   return (
     <div className="container-all">
       {/* bg-img */}
       <div className="container-left"></div>
-
       {/* content */}
       <div className="container-right">
         <form className="content">
           <div className="header">
             <h1 className="title loginTitle1">Reset password</h1>
           </div>
-
-          <ResetForm />
-          <Buttons />
+          <ResetForm
+            email={email}
+            setEmail={setEmail}
+            verificationCodeSent={verificationCodeSent}
+            setVerificationCodeSent={setVerificationCodeSent}
+            verificationCode={verificationCode}
+            setEnteredCode={setEnteredCode}
+          />
+          <Buttons handleVerifyCode={handleVerifyCode} />
         </form>
       </div>
     </div>
   );
 }
 
-function ResetForm() {
-  const [email, setEmail] = useState("");
-  const [verificationCodeSent, setVerificationCodeSent] = useState(false);
-
-  //generate vertification code
-  const verificationCode = Math.floor(
-    100000 + Math.random() * 900000
-  ).toString();
-
+function ResetForm(props) {
   const handleGetCodeClick = () => {
     emailjs
       .send(
@@ -40,28 +60,34 @@ function ResetForm() {
         "template_jfx4wqp",
         {
           from_name: "My Circle",
-          email_from: email, //user email
-          verification_code: verificationCode,
+          email_from: props.email,
+          verification_code: props.verificationCode,
           reply_to: "bbzhuoying@gmail.com",
         },
         "qONCoXhfVWko-j4vy"
       )
       .then((response) => {
         console.log("Email sent successfully:", response);
-        setVerificationCodeSent(true);
+        props.setVerificationCodeSent(true);
       })
       .catch((error) => {
         console.error("Error sending email:", error);
       });
   };
+
   return (
     <div>
-      <InputForm inputTitle="Email Address"  onChange={e => setEmail(e.target.value)} />
+      <InputForm
+        inputTitle="Email Address"
+        onChange={(e) => props.setEmail(e.target.value)}
+      />
       <div className="vertification">
         <div className="vertification-left">
-          <InputForm inputTitle="Email Verification Code" />
+          <InputForm
+            inputTitle="Email Verification Code"
+            onChange={(e) => props.setEnteredCode(e.target.value)}
+          />
         </div>
-
         <div>
           <button
             type="button"
@@ -78,36 +104,18 @@ function ResetForm() {
   );
 }
 
-function Buttons() {
-  const navigate = useNavigate();
-
-  const handleLogInClick = () => {
-    navigate("/login");
-  };
-
+function Buttons(props) {
   return (
     <form className="input">
       <button
         type="button"
         className="btn btn-lighter-secondary w-100 rounded-5 py-2 login"
-        onClick={handleLogInClick}
+        onClick={props.handleVerifyCode}
       >
         Reset
       </button>
     </form>
   );
-}
-
-function SendEmailForm() {
-  const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs.sendForm(
-      "service_pduei9j",
-      "template_7nityb8",
-      e.target,
-      "qONCoXhfVWko-j4vy"
-    );
-  };
 }
 
 export default LoadForgotPage;

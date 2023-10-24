e99e591c075ed0877ba50d216519162e3535551c
Zi Tao (Chloe) <chloetaozi@gmail.com>
2023-10-25 00:44:22 +1100

    adjust event summary timing

--- src/pages/DashnoardPage/Dashboard.js
+++ src/pages/DashnoardPage/Dashboard.js
@@ -168,8 +168,8 @@ function LoadDashboardPage() {
       };
   
       const formatTime = date => {
-        const hours = String(date.getUTCHours() - 1).padStart(2, '0');
-        const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
         return `${hours}:${minutes}`;
       };
   
